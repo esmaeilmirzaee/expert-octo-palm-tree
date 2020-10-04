5bb63eb77bf58baa1f3839ddca1e18cc37f5a2ec
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-10-04 08:27:58 -0400

    Colour adjuster refactor leveraging Reducer + manage condition

--- src/screens/SquareScreen.jsx
+++ src/screens/SquareScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { step0 } from 'react-native/Libraries/Animated/src/Easing';
 
 import ColourCounter from '../components/ColourCounter';
 
@@ -8,11 +9,18 @@ const COLOUR_INCREMENT = 25;
 const reducer = (state, action) => {
   switch (action.colour) {
     case 'red':
-      return { ...state, red: state.red + action.amount };
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
     case 'green':
-      return { ...state, green: state.green + action.amount };
+      return state.green + action.amount > 255 ||
+        state.green + action.amount < 0
+        ? state
+        : { ...state, green: state.green + action.amount };
     case 'blue':
-      return { ...state, blue: state.blue + action.amount };
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
     default:
       return state;
   }
