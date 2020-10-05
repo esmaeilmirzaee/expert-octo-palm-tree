0885005b1bf4be7d6a3e8d2ef756ab64cdacf77a
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-10-05 09:32:00 -0400

    Experiment styling | absoluteFillObject

--- src/screens/BoxScreen.jsx
+++ src/screens/BoxScreen.jsx
@@ -5,7 +5,11 @@ const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
       <Text style={styles.textStyle}>Box</Text>
-      <Text style={styles.textStyle}>Box</Text>
+      <Text
+        style={{ ...StyleSheet.absoluteFillObject, backgroundColor: 'black' }}
+      >
+        Box
+      </Text>
       <Text style={styles.textStyle}>Box</Text>
     </View>
   );
@@ -16,10 +20,14 @@ const styles = StyleSheet.create({
     borderColor: 'blue',
     borderWidth: 1,
     padding: 5,
+    height: 200,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   textStyle: {
     borderWidth: 1,
     borderColor: 'red',
+    alignSelf: 'center',
   },
 });
 
