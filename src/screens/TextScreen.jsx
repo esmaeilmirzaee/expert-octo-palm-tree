1384f5d5d3e5ac5ef9c25e007a747e654da613b8
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-10-05 03:40:05 -0400

    Experiment text input

--- src/screens/TextScreen.jsx
+++ src/screens/TextScreen.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, Text } from 'react-native';
 
 const TextScreen = () => {
+  const [name, setName] = useState('');
   return (
     <View>
       <TextInput
         style={styles.input}
         autoCapitalize='none'
         autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
       />
+      <Text style={{ fontSize: 80 }}>{name}</Text>
     </View>
   );
 };
